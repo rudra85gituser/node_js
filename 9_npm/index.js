a64d4f7f0ad6dadf9197e3eb5ebdb988accda30e
Rudra Yadav <realrudrayadav@gmail.com>
2024-04-10 23:52:57 +0530

    npm tutorial

---
+++ 9_npm/index.js
@@ -0,0 +1,27 @@
+//stands for node package manager
+//how we can do form validation and using package how we can do changes in terminal output
+//java script have highest npm
+// we have to import a validator package
+
+
+//firstly initialize npm module
+//esc + ctrl + `
+
+// all the packages which we store using npm will get stored in packsge.json as dependencies
+//clearcut meaning  = while doing a project package.jsn stores all information of packages used / inatalled during project
+//all project and libraries get stored in package.jsn++
+
+//try to initialize it in root folder
+
+//this holds more information about chalk module
+
+//1
+//chalk package = Terminal string styling done using chalk , to make terminal colorful
+
+//package lock.json  this holds more information about chalk module
+
+//node module stores all packages information
+
+const chalk = require ("chalk");
+console.log(blue("hello world!"));
+
